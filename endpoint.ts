64f5f34b0daeda3c26abd865ca4a83d5595837ce
biginfo2012 <biginfo2012@gmail.com>
2024-06-14 23:04:21 +0200

    update amount as decimals

--- endpoint.ts
+++ endpoint.ts
@@ -1,13 +1,15 @@
 import Web3 from 'web3';
 import BigNumber from 'bignumber.js';
 import dotenv from 'dotenv';
+import { parseUnits } from 'ethers';
 
 dotenv.config();
 
 const buyerArtifact = require('./artifacts/PancakeBuyer.json');
 const erc20Artifact = require('./artifacts/ERC20.json');
 
-const web3 = new Web3(process.env.BSC_TESTNET_RPC_URL);
+const rpcUrl = process.env.ENV == 'MAINNET' ? process.env.BSC_MAINNET_RPC_URL : process.env.BSC_TESTNET_RPC_URL;
+const web3 = new Web3(rpcUrl);
 const address = process.env.CONTRACT_ADDRESS;
 const contract = new web3.eth.Contract(buyerArtifact.abi, address);
 
@@ -18,25 +20,31 @@ export async function v2swap(
     tokenIn: string,
     tokenOut: string,
     recipient: string,
-    amountOut: number,
-    amountInMaximum: number
+    amountOut: string,
+    amountInMaximum: string
 ) {
     try {
         console.log('Transfer approving is requested.');
         const tokenInContract = new web3.eth.Contract(erc20Artifact.abi, tokenIn);
-        // approve token transfer for buyer contract
-        await tokenInContract.methods.approve(address, amountInMaximum).send({from: myAddress});
+        const tokenOutContract = new web3.eth.Contract(erc20Artifact.abi, tokenOut);
+        const decimalsIn = await tokenInContract.methods.decimals().call();
+        const decimalsOut = await tokenOutContract.methods.decimals().call();
 
+        const amountOutN = parseUnits(amountOut, Number(decimalsOut));
+        const amountInMaximumN = parseUnits(amountInMaximum, Number(decimalsIn));
+        // approve token transfer for buyer contract
+        await tokenInContract.methods.approve(address, amountInMaximumN).send({from: myAddress});
         console.log('Token transfer is approved.');
 
-        const response = await contract.methods.swapTokensForExactTokens(
-            amountOut,
-            amountInMaximum,
+        console.log("Swapping is requested");
+        await contract.methods.swapTokensForExactTokens(
+            amountOutN,
+            amountInMaximumN,
             [tokenIn, tokenOut],
             recipient
         ).send({from: myAddress});
         
-        console.log("Success");
+        console.log("Success swapping");
     } catch(error) {
         console.error("Error swap v2 pair:", error);
         throw error;
@@ -48,33 +56,40 @@ export async function v3swap(
     tokenOut: string,
     fee: number,
     recipient: string,
-    amountOut: number,
-    amountInMaximum: number,
+    amountOut: string,
+    amountInMaximum: string,
     sqrtPriceLimitX96: number
 ) {
-    const param = {
-        tokenIn,
-        tokenOut,
-        fee,
-        recipient,
-        amountOut,
-        amountInMaximum,
-        sqrtPriceLimitX96
-    };
 
     try {
         console.log('Transfer approving is requested.');
         const tokenInContract = new web3.eth.Contract(erc20Artifact.abi, tokenIn);
-        // approve token transfer for buyer contract
-        await tokenInContract.methods.approve(address, amountInMaximum).send({from: myAddress});
+        const tokenOutContract = new web3.eth.Contract(erc20Artifact.abi, tokenOut);
+        const decimalsIn = await tokenInContract.methods.decimals().call();
+        const decimalsOut = await tokenOutContract.methods.decimals().call();
 
+        const amountOutN = parseUnits(amountOut, Number(decimalsOut));
+        const amountInMaximumN = parseUnits(amountInMaximum, Number(decimalsIn));
+        // approve token transfer for buyer contract
+        await tokenInContract.methods.approve(address, amountInMaximumN).send({from: myAddress});
         console.log('Token transfer is approved.');
 
-        const trx = await contract.methods.exactOutputSingle(
+        const param = {
+            tokenIn,
+            tokenOut,
+            fee,
+            recipient,
+            amountOutN,
+            amountInMaximumN,
+            sqrtPriceLimitX96
+        };
+
+        console.log("Swapping is requested");
+        await contract.methods.exactOutputSingle(
             param
         ).send({from: myAddress});
 
-        console.log("Success");
+        console.log("Success swapping");
     } catch(error) {
         console.error("Error swap v3 pair:", error);
         throw error;
@@ -82,7 +97,7 @@ export async function v3swap(
 }
 
 export async function test() {
-    console.log(BigNumber(3).times(4).pow(2));
+    console.log(parseUnits('0.34', 18));
 }
 
 function convert(

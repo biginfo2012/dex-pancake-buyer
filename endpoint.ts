da9e4bb3f35688429a04d0eeb4307a6707d36bf3
biginfo2012 <biginfo2012@gmail.com>
2024-06-14 20:16:26 +0200

    init commit

---
+++ endpoint.ts
@@ -0,0 +1,102 @@
+import Web3 from 'web3';
+import BigNumber from 'bignumber.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const buyerArtifact = require('./artifacts/PancakeBuyer.json');
+const erc20Artifact = require('./artifacts/ERC20.json');
+
+const web3 = new Web3(process.env.BSC_TESTNET_RPC_URL);
+const address = process.env.CONTRACT_ADDRESS;
+const contract = new web3.eth.Contract(buyerArtifact.abi, address);
+
+const account = web3.eth.accounts.wallet.add('0x'+String(process.env.PRIVATE_KEY));
+const myAddress = account[0].address;
+
+export async function v2swap(
+    tokenIn: string,
+    tokenOut: string,
+    recipient: string,
+    amountOut: number,
+    amountInMaximum: number
+) {
+    try {
+        console.log('Transfer approving is requested.');
+        const tokenInContract = new web3.eth.Contract(erc20Artifact.abi, tokenIn);
+        // approve token transfer for buyer contract
+        await tokenInContract.methods.approve(address, amountInMaximum).send({from: myAddress});
+
+        console.log('Token transfer is approved.');
+
+        const response = await contract.methods.swapTokensForExactTokens(
+            amountOut,
+            amountInMaximum,
+            [tokenIn, tokenOut],
+            recipient
+        ).send({from: myAddress});
+        
+        console.log("Success");
+    } catch(error) {
+        console.error("Error swap v2 pair:", error);
+        throw error;
+    };
+}
+
+export async function v3swap(
+    tokenIn: string,
+    tokenOut: string,
+    fee: number,
+    recipient: string,
+    deadline: number,
+    amountOut: number,
+    amountInMaximum: number,
+    sqrtPriceLimitX96: number
+) {
+    const param = {
+        tokenIn,
+        tokenOut,
+        fee,
+        recipient,
+        deadline,
+        amountOut,
+        amountInMaximum,
+        sqrtPriceLimitX96
+    };
+
+    try {
+        console.log('Transfer approving is requested.');
+        const tokenInContract = new web3.eth.Contract(erc20Artifact.abi, tokenIn);
+        // approve token transfer for buyer contract
+        await tokenInContract.methods.approve(address, amountInMaximum).send({from: myAddress});
+
+        console.log('Token transfer is approved.');
+
+        const trx = await contract.methods.exactOutputSingle(
+            param
+        ).send({from: myAddress});
+
+        console.log("Success");
+    } catch(error) {
+        console.error("Error swap v3 pair:", error);
+        throw error;
+    };
+}
+
+export async function test() {
+    console.log(BigNumber(3).times(4).pow(2));
+}
+
+function convert(
+    num: BigNumber,
+    precision: number,
+    round: BigNumber.RoundingMode = 1
+): string {
+    if (num.gte(1)) return num.toFixed(precision, 1);
+
+    const exponent = new BigNumber(Number(num.e));
+    return num.toFixed(
+        exponent.abs().plus(precision - 1).toNumber(),
+        round
+    );
+}
---
+++ main.ts
@@ -0,0 +1,52 @@
+import {
+    v2swap,
+    v3swap,
+    test
+} from "./endpoint";
+
+function doTest() {
+    console.log("In progress of test.....");
+    test();
+}
+
+const functionIndicator = process.argv[2];
+
+if (functionIndicator == '2') {
+    // swap on the v2 pool
+    const tokenIn = process.argv[3];
+    const tokenOut = process.argv[4];
+    const recipient = process.argv[5];
+    const amountOut = Number(process.argv[6]);
+    const amountInMaximum = Number(process.argv[7]);
+
+    v2swap(
+        tokenIn,
+        tokenOut,
+        recipient,
+        amountOut,
+        amountInMaximum
+    );
+} else if (functionIndicator == '3') {
+    // swap on the v3 pool
+    const tokenIn = process.argv[3];
+    const tokenOut = process.argv[4];
+    const recipient = process.argv[5];
+    const amountOut = Number(process.argv[6]);
+    const amountInMaximum = Number(process.argv[7]);
+    const fee = Number(process.argv[8]);
+    const deadline = Number(process.argv[9]);
+    const sqrtPriceLimitX96 = Number(process.argv[10]);
+
+    v3swap(
+        tokenIn,
+        tokenOut,
+        fee,
+        recipient,
+        deadline,
+        amountOut,
+        amountInMaximum,
+        sqrtPriceLimitX96
+    );
+} else {
+    doTest();
+}
